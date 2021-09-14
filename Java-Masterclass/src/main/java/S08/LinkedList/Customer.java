9c17501ef500c8892b5927225b7b7fb7054d2521
Muhammed <mxgardeee@gmail.com>
2021-09-14 19:25:22 +0200

    Arrays, Java inbuilt Lists, Autoboxing and Unboxing

---
+++ Java-Masterclass/src/main/java/S08/LinkedList/Customer.java
@@ -0,0 +1,32 @@
+package S08.LinkedList;
+
+/**
+ *
+ * @author 27605(Muhammed Gardee)
+ */
+
+public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
