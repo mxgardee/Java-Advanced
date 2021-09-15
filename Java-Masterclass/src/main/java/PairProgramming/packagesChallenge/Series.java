b58bcb8ac2caae9ed7b0e28c94cc44df15c32ccb
Muhammed <mxgardeee@gmail.com>
2021-09-15 13:21:30 +0200

    Pair Programming, S10, Muhammed Gardee(Driver), Rossouw Venter(Nav), James Stromnes(Nav)

---
+++ Java-Masterclass/src/main/java/PairProgramming/packagesChallenge/Series.java
@@ -0,0 +1,49 @@
+package packagesChallenge;
+
+/**
+ *
+ * @author 27605 (Muhammed Gardee)
+ */
+
+// Pair Programming: Muhammed Gardee (Driver)
+//                   Rossouw Venter  (Navigator)
+//                   James Stromnes  (Navigator)
+
+public class Series {
+    
+    public static long nSum(int n) {
+        return (n * (n + 1)) /2;
+    }
+    
+    public static long factorial(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        long f = 1;
+        for (int i=1; i <= n; i++) {
+            f *= i;
+        }
+        
+        return f;
+    }
+
+    public static long fib(int n) {
+        if(n == 0) {
+            return 0;
+     
+    } else if (n == 1) {
+        return 1;
+    }
+        long nMinus1 = 1;
+        long nMinus2 = 0;
+        long fibNum = 0;
+        
+        for (int i = 1; i<n; i++) {
+            fibNum = (nMinus2 + nMinus1);
+            nMinus2 = nMinus1;
+            nMinus1 = fibNum;
+        }
+        return fibNum;
+    }
+
+}
