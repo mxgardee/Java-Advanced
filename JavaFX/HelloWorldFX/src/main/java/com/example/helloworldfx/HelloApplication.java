3ddf510d604aec41dd90803a08990bbdec33f37b
Muhammed <mxgardeee@gmail.com>
2021-09-24 09:33:54 +0200

    JavaFX

--- JavaFX/HelloWorldFX/src/main/java/com/example/helloworldfx/HelloApplication.java
+++ JavaFX/HelloWorldFX/src/main/java/com/example/helloworldfx/HelloApplication.java
@@ -2,15 +2,10 @@ package com.example.helloworldfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -19,20 +14,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-
-//        GridPane root = new GridPane();
-//        root.setAlignment(Pos.CENTER);
-//        root.setVgap(10);
-//        root.setHgap(10);
-//
-//        Label greeting = new Label("Welcome to JavaFX!");
-//        greeting.setTextFill(Color.GREEN);
-//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-//
-//        root.getChildren().add(greeting);
-
-        primaryStage.setTitle("Hello JavaFX!");
-        primaryStage.setScene(new Scene(root, 700, 275));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 500, 275));
         primaryStage.show();
     }
 
@@ -41,3 +24,4 @@ public class HelloApplication extends Application {
         launch(args);
     }
 }
+
