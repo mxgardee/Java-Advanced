90f332239a053751fcd64d72d9aa061262d01702
Muhammed <mxgardeee@gmail.com>
2021-10-11 10:37:06 +0200

    JavaFX

---
+++ JavaFX/SceneBuilder/Controller.java
@@ -0,0 +1,15 @@
+package com.example.scenebuilder;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    public void handleAction() {
+        label.setText("OK Button pressed");
+    }
+}
